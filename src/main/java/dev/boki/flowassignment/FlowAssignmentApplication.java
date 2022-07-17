77797190647d7d5c8fefdf6dca032a52017024b4
boki <lsb530@naver.com>
2022-07-17 23:36:45 +0900

    indexController 추가

--- src/main/java/dev/boki/flowassignment/FlowAssignmentApplication.java
+++ src/main/java/dev/boki/flowassignment/FlowAssignmentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowAssignmentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FlowAssignmentApplication.class, args);
     }
